cbaa93c98e26b3814a534b7284cf651f9dbb80b2
luciamam <lumanmba2023@gmail.com>
2024-10-26 00:15:19 +0200

    la version pc  está  lista

--- scripts/script.js
+++ scripts/script.js
@@ -11,16 +11,13 @@ let datoedad
 let dato_nacionalidad
 
 let mostrarDatos=()=>{
-
     seccionDatos.style.display='none'
     seccion_mostrarDatos.style.display='flex'
     divMostrarDatos.innerHTML=`
     <h3 class='dato'>Género: ${datogenero}</h3>
-    <h3 class='dato'>Edad:${datoedad}</h3>
-    <h3 class='dato'>Nacionalidad:${dato_nacionalidad}</h3>
+    <h3 class='dato'>Edad: ${datoedad} años</h3>
+    <h3 class='dato'>Nacionalidad: ${dato_nacionalidad}</h3>
     `
-
-    
 }
 
 bonton_buscar.addEventListener('click',async()=>{
@@ -30,38 +27,23 @@ bonton_buscar.addEventListener('click',async()=>{
         return alert("Debe ser mayor de 3 carateres")
     }
 
-
-
     let endpoint_edad=`https://api.agify.io?name=${nombre}`
     let res= await fetch(endpoint_edad)
     let dato= await res.json()
     datoedad = dato.age
 
-
     let endpoint_nacionalidad=`https://api.nationalize.io/?name=${nombre}`
     let res1=await fetch(endpoint_nacionalidad)
     let datoN=await res1.json()
     dato_nacionalidad=datoN.country[0].country_id
+
     console.log(dato_nacionalidad);
-    
-    
-
-
     let endpointgenero=`https://api.genderize.io?name=${nombre}`
     let res2= await fetch(endpointgenero)
     let datogenero_Objeto = await res2.json()
     //console.log(datogenero_Objeto);
 
-
-    let endpointedad=
-
-
-
-    
     datogenero=datogenero_Objeto.gender
-
-
-
     mostrarDatos()
     
     
